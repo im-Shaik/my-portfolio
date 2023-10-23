1f9b2434c81bd69c604ab64a8307686f8a1897d7
sh-mohamed-faizal <mohamedfaizal813@gmail.com>
2023-10-23 15:12:02 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 4000);
+    }, 3000);
   }, []);
 
   return (
--- src/context/Data.js
+++ src/context/Data.js
@@ -13,35 +13,35 @@ export function DataContextProvider({ children }) {
       siteIMG:
         "https://www.reshot.com/preview-assets/illustrations/W6CRPJNKST/isometric-travel-app-illustration-W6CRPJNKST-w1600.jpg",
       siteName: "Travino Travel",
-      siteURL: "https://mohamedfaizal813.github.io/Travel-Concept/",
+      siteURL: "https://im-shaik.github.io/Travel-Concept/",
     },
     {
       id: 2,
       siteIMG:
         "https://www.reshot.com/preview-assets/illustrations/WDR7FKLXPH/marketing-online-WDR7FKLXPH-w1600.jpg",
       siteName: "Besnik Market",
-      siteURL: "https://mohamedfaizal813.github.io/Besnik-market-concept/",
+      siteURL: "https://im-shaik.github.io/Besnik-market-concept/",
     },
     {
       id: 3,
       siteIMG:
         "https://www.reshot.com/preview-assets/illustrations/NTK76WHCL5/server-network-technology-NTK76WHCL5-w1600.jpg",
       siteName: "NetBook-Concept",
-      siteURL: "https://mohamedfaizal813.github.io/NetBook-concept/",
+      siteURL: "https://im-shaik.github.io/NetBook-concept/",
     },
     {
       id: 4,
       siteIMG:
         "https://www.reshot.com/preview-assets/illustrations/K5VSWPFRC7/website-design-team-K5VSWPFRC7-w1600.jpg",
       siteName: "Sassify-Concept",
-      siteURL: "https://mohamedfaizal813.github.io/Sassify-concept/",
+      siteURL: "https://im-shaik.github.io/Sassify-concept/",
     },
     {
       id: 5,
       siteIMG:
         "https://www.reshot.com/preview-assets/illustrations/65VK7FUGS2/school-bus-boy-65VK7FUGS2-w1600.jpg",
       siteName: "High-School",
-      siteURL: "https://mohamedfaizal813.github.io/School-concept/",
+      siteURL: "https://im-shaik.github.io/School-concept/",
     },
     {
       id: 6,
